ea6241cfc76162c6196f66360ffee07fcd2629d2
SerbanHiro <51057957+SerbanHiro@users.noreply.github.com>
2022-08-29 15:00:31 +0300

    Initial commit

---
+++ src/main/java/net/serbob/mod_tutorial/ModTutorial.java
@@ -0,0 +1,14 @@
+package net.serbob.mod_tutorial;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModTutorial implements ModInitializer {
+	public static final String MOD_ID = "mod_tutorial";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/serbob/mod_tutorial/ModTutorialClient.java
@@ -0,0 +1,10 @@
+package net.serbob.mod_tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTutorialClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
