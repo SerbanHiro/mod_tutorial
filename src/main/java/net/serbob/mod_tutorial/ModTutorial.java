d9852fe26462e2ea2f8aa711f175472ce0c6c0eb
SerbanHiro <51057957+SerbanHiro@users.noreply.github.com>
2022-08-29 15:02:07 +0300

    Am adaugat o linie noua aka Dorian Popa!

--- src/main/java/net/serbob/mod_tutorial/ModTutorial.java
+++ src/main/java/net/serbob/mod_tutorial/ModTutorial.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ModTutorial implements ModInitializer {
+	// dorian popa
 	public static final String MOD_ID = "mod_tutorial";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
